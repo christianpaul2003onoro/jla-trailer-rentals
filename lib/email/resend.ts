6a3790af705cc9327506cf4ac7ab183210e08bbf
christianpaul2003onoro <christianpaul2003onoro@gmail.com>
2025-11-05 17:24:44 -0500

    Update resend.ts

--- lib/email/resend.ts
+++ lib/email/resend.ts
@@ -1,1 +1,9 @@
 
+// lib/email/resend.ts
+import { Resend } from 'resend';
+
+if (!process.env.RESEND_API_KEY) {
+  throw new Error('Missing RESEND_API_KEY env var');
+}
+
+export const resend = new Resend(process.env.RESEND_API_KEY);
